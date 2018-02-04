31abcf5401189f9eec9f479f1b0365145d7dd504
Joshua Peter Ebenezer <JoshuaEbenezer@users.noreply.github.com>
2018-02-04 13:50:16 +0530

    Correct addressing

--- zl78.py
+++ zl78.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import binascii
 
 def find_phrase(databyte,phrases):
 	try:
@@ -10,38 +10,45 @@ def find_phrase(databyte,phrases):
 		return found	
 
 
+def text_from_bits(bits, encoding='utf-8', errors='surrogatepass'):
+    n = int(bits, 2)
+    return n.to_bytes((n.bit_length() + 7) // 8, 'big').decode(encoding, errors) or '\0'
+
+
 phrase_count = 0
 phrases = []
 address = []
-for number in range(1024):
-	address.append("{0:b}".format(number))
+for number in range(4096):
+	address.append(format(number,'012b'))
 codeword = []
 
 data_length = 0
 code_length = 0
 
 f = open("data.txt", "rb")
+fout = open("code.txt","w")
 databyte = f.read(1)
-index = 0
 while databyte:
-	new_phrase = "{0:b}".format(ord(databyte))
-	data_length += len(databyte)
+	index = 0
+	new_phrase = format(ord(databyte),'08b')
 	found = find_phrase(new_phrase,phrases)
 	while (found):
 		index = found
 		databyte = f.read(1)
 		if (databyte):
-			new_phrase = new_phrase + "{0:b}".format(ord(databyte))
+			new_phrase = new_phrase + format(ord(databyte),'08b')
 			found = find_phrase(new_phrase,phrases)
-			data_length += len(databyte)
 		else:
 			found = False
 			break
-
+	data_length += len(new_phrase)
 	if (databyte):
 		phrase_count+=1
-		new_codeword = address[index] + "{0:b}".format(ord(databyte))
-		print ('Phrase count : ', phrase_count, 'New phrase : ', new_phrase, 'New codeword : ', new_codeword)
+		new_codeword = address[index] + format(ord(databyte),'08b')
+		print ('Phrase count : ', phrase_count, 'New phrase : ', new_phrase, 'New codeword : ', new_codeword, 'Address : ', address[phrase_count])
+		eng = text_from_bits(new_phrase)
+		print('New phrase in English : ', eng)
+		fout.write(new_codeword)
 		phrases.append(new_phrase)
 		codeword.append(new_codeword)
 		databyte = f.read(1)
@@ -50,5 +57,5 @@ while databyte:
 print('Encoding completed...')
 for word in codeword:
 	code_length += len(word)
-efficiency = (1-data_length/code_length) * 100
+efficiency = (1-code_length/data_length) * 100
 print('Efficiency is : ', efficiency,'%')
